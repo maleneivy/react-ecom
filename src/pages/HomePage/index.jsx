7d6813370855436861344459e31df1c30ec62070
maleneivy <68667843+maleneivy@users.noreply.github.com>
2024-03-12 12:26:39 +0100

    Fetched list from api, displaying on HomePage with image, title and price and discounted price

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,6 +1,41 @@
+import Loader from "../../components/Loader";
+import { useApi } from "../../hooks/useApi";
+import * as S from "./index.styles";
+
 function Home() {
+    const { data, isLoading, isError} = useApi('https://v2.api.noroff.dev/online-shop');
+    console.log(data);
+
+    if(isLoading) {
+        return <Loader />
+    }
+
+    if(isError) {
+        return <div>Error</div>
+    }
     return (
-        <h1>Home</h1>
+        <main>
+            <S.Heading>Products</S.Heading>
+            <S.ProductsContainer>
+                {data && data.map((product) => (
+                    <S.ProductCard key={product.id}>
+                        <S.ProductImage src={product.image?.url} alt={product.title} />
+                        <S.ProductTitle>{product.title}</S.ProductTitle>
+                        <S.ProductPrice>
+                            {product.price === product.discountedPrice ? (
+                                <span>{product.price}</span>
+                            ) : (
+                            <div className="discounted-price">
+                                <span className="on-sale-price">{product.discountedPrice} NOK</span>
+                                <span className="old-price">Originally: {product.price}</span>
+                            </div>
+                            )}
+                        </S.ProductPrice>
+                    </S.ProductCard>
+                ))}
+            </S.ProductsContainer>
+
+        </main>
     );
 }
 
---
+++ src/pages/HomePage/index.styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const ProductsContainer = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+margin: 0 auto;
+`;
+
+export const ProductCard = styled.div`
+display: flex;
+flex-direction: column;
+margin: 2rem;
+width: calc(33% - 2rem);
+max-width: 300px; 
+`;
+
+export const Heading = styled.h1`
+margin-bottom: 1rem; 
+`
+
+export const ProductImage = styled.img`
+max-width: 100%;
+height: 200px;
+object-fit: cover;
+`;
+
+export const ProductTitle = styled.h3`
+margin-bottom: 0.5rem;
+`;
+
+export const ProductPrice = styled.h4`
+margin-bottom: 0,5rem;
+
+.discounted-price {
+    display: flex; 
+    flex-direction: column;
+}
+
+.old-price {
+    text-decoration: line-through;
+}
+
+.on-sale-price {
+    color: red;
+}
+`;
